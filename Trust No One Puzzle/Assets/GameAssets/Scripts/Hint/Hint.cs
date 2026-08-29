75855ba0f5f68913a7fe35f03e168344bccdae79
makakrrik <107197581+makakrrik@users.noreply.github.com>
2026-08-29 19:10:30 +0700

    WrongHintSystem

---
+++ Trust No One Puzzle/Assets/GameAssets/Scripts/Hint/Hint.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GameAssets.Scripts.Hint
+{
+    public class Hint : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text hintText;
+        public string Text
+        {
+            get => hintText.text;
+            set
+            {
+                if (value != string.Empty)
+                {
+                    hintText.text = value;
+                }
+            }
+        }
+    }
+}
